9f5984b0cf9e63a495090bc833b69cc9aa7ae475
Johannes Steurer <johannes.steurer@student.tugraz.at>
2012-10-29 11:07:47 +0100

    test

--- apo/ex1/ex_1_1_tests.py
+++
@@ -1,6 +0,0 @@
-from test_utils import MlTutorTestCase
-
-
-class TestExercise1(MlTutorTestCase):
-    assert_equal = ('x', 'y', 'summe', 'differenz', 'produkt', 'quotient',
-                    'potenz', 'wurzel', 'root3', 'trig1', 'trig2', 'trig3')
--- apo/ex1/ex_1_2_tests.py
+++
@@ -1,5 +0,0 @@
-from test_utils import MlTutorTestCase
-
-
-class TestExercise2(MlTutorTestCase):
-    assert_equal = ('x_start', 'x_end', 'x_delta', 'x_1', 'y_1', 'x_2', 'y_2')
