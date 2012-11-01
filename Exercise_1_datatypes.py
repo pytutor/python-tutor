7e8f214987817db8eb1817eafd4642b01353ff22
peter boehling <boehlingpeter@googlemail.com>
2012-11-01 16:47:25 +0100

    exercises
    muss noch die erklärung schreiben

---
+++ Exercise_1_datatypes.py
@@ -0,0 +1,13 @@
+# built in data types of Python and ist casesensitie
+
+IntegerNumber = 42
+FloatNumber = 3.1415
+floatnumber = 2.718
+String = 'Hello World'
+OtherString = "Bye World"
+
+a_list = range(10)
+a_tuple = ([2,a_list,4,5])
+
+
+print(IntegerNumber, FloatNumber, floatnumber, String,OtherString, a_list,a_tuple)
