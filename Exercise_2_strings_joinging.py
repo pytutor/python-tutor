2506187cd40c1b49afb3cee1e2afd1f9482622cc
Johannes Steurer <johannes.steurer@student.tugraz.at>
2013-01-07 17:56:21 +0100

    some files deleted

--- Exercise_2_strings_joinging.py
+++
@@ -1,9 +0,0 @@
-# learn how to create strings
-
-name = 'Name'
-Number = 30
-Endung = 'txt'
-
-string_name = name + '%0.4d' %Number + '.' + Endung
-string_name2 = name + str(Number) + '.' + Endung
-print(string_name)
